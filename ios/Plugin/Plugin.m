77ddb1ff69f6dbff33afdb39c229f6236d6be785
chaw <cshen_581@hotmail.com>
2020-07-09 17:55:42 +0800

    setting page for iOS

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -5,4 +5,9 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(CapacitorPusherBeamsAuth, "CapacitorPusherBeamsAuth",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(onlyAndroid, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(openWebVersion, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(goToAppSetting, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(openMap, CAPPluginReturnPromise);
+           
 )
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -15,10 +15,62 @@ public class CapacitorPusherBeamsAuth: CAPPlugin {
         ])
     }
 
+    @objc func onlyAndroid(_ call: CAPPluginCall) {
+        let value = call.getString("value") ?? ""
+        call.success([
+            "value": value
+        ])
+    }
+    
+    @objc func openWebVersion(_ call: CAPPluginCall) {
+        let value = call.getString("value") ?? ""
+        call.success([
+            "value": value
+        ])
+    }
+    
+    @objc func goToAppSetting(_ call: CAPPluginCall) {
+        let value = call.getString("value") ?? ""
+        
+        let alertController = UIAlertController (
+        	title: "Push Notification", 
+        	message: "Allow push notification permission on settings?", 
+        	preferredStyle: .alert
+        )
+
+        let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) -> Void in
+
+            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                return
+            }
+
+            if UIApplication.shared.canOpenURL(settingsUrl) {
+                UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                    print("Settings opened: \(success)") // Prints true
+                })
+            }
+        }
+        alertController.addAction(settingsAction)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alertController.addAction(cancelAction)
+
+        DispatchQueue.main.async {
+          self.bridge.viewController.present(alertController, animated: true, completion: nil)
+        }
+        
+        call.success([
+            "value": value
+        ])
+    }
+
     @objc func openMap(_ call: CAPPluginCall) {
-		let latitude = call.getString("latitude")
-		let longitude = call.getNumber("longitude")
-		print(latitude)
+        let latitude:String? = call.getString("latitude")
+        let longitude:Double? = call.getDouble("longitude")
+        print(latitude ?? "default value")
+        print(longitude ?? "default value")
 		// more logic
+        call.resolve([
+            "value": latitude ?? "<#default value#> "+String(format:"%.1f", longitude!)
+        ]);
 	}
 }
